591ddd79e78a1c20277f61582510f5858a453831
EstebanS96 <33339247+EstebanS96@users.noreply.github.com>
2019-09-15 19:39:50 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using EFCodingC.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFCodingC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<UserViewModel> lst = null;
+            using (Models.codeChallengeEntities db = new Models.codeChallengeEntities())
+            {
+                lst =
+                    (from d in db.Users
+                     select new UserViewModel
+                     {
+                         id = d.id,
+                         firstName = d.firstName + " " + d.lastName
+                     }).ToList();
+            }
+            List<SelectListItem> items = lst.ConvertAll(x =>
+            {
+                return new SelectListItem()
+                {
+                    Text = x.firstName.ToString(),
+                    Value = x.id.ToString(),
+                    Selected = false
+                };
+            });
+
+            ViewBag.items = items;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EFCodingC.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+    }
+}
